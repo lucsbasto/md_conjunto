09451b53591ab5097cd3f84fea1df3f5e12c1348
pablohenriques <41550392+pablohenriques@users.noreply.github.com>
2020-02-09 15:31:44 -0300

    Arquivos corrigidos
    Correção dos programas, conforme solicitado no exercício.

--- conjunto.py
+++ conjunto.py
@@ -6,15 +6,14 @@ class Conjunto():
 		self.nome = nome
 		
 	def inserirElemento(self, valor):
-		if valor in self.lista:
-			return False
-		self.lista.append(valor) 
+		if valor not in self.lista:
+			self.lista.append(valor) 
 
 	def tamanhoConjunto(self):
 		return len(self.lista)
 
 	def imprimir(self):
-		print(f'Nome conjunto: {self.nome} \n Conjunto:{self.lista}')
+		print(f'{self.nome.upper()} = {self.lista}')
 	
 		
 #####################################
--- mainColecao.py
+++ mainColecao.py
@@ -1,17 +1,21 @@
 import conjunto 
 
 c1 = conjunto.Conjunto('A')
-c1.inserirElemento('B')
-c1.inserirElemento('B')
-c1.inserirElemento('A')
-c1.inserirElemento('A')
+c2 = conjunto.Conjunto('A')
 
+c1.inserirElemento('B')
+c1.inserirElemento('B')
+c1.inserirElemento('A')
+c1.inserirElemento('A')
 
 print("Tamanho da Coleção:",c1.tamanhoConjunto())
-
 c1.imprimir()
 
+c2.inserirElemento('D')
+c2.inserirElemento(c1.lista)
 
+print("Tamanho da Coleção:",c2.tamanhoConjunto())
+c2.imprimir()
 
 #####################################
 
