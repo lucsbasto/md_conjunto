bc06ff24b045716862c28a536a6985730837abae
adm.palmas2 <adm.palmas2@ensino.ulbra.net>
2020-02-19 11:43:17 -0300

    Upgrade_Atividade2

--- conjunto.py
+++ conjunto.py
@@ -4,6 +4,9 @@ class Conjunto():
 	def __init__(self, nome):
 		self.lista = []
 		self.nome = nome
+
+	def conjunto(self):
+		return self.lista
 		
 	def inserirElemento(self, valor):
 		if valor not in self.lista:
@@ -12,8 +15,40 @@ class Conjunto():
 	def tamanhoConjunto(self):
 		return len(self.lista)
 
+	def pertence(self, elemento, conjunto):
+		if elemento not in conjunto:
+			return False
+		return True
+
+	def eh_subconjunto(self, conjunto1, conjunto2):
+		for i in conjunto1:
+			#print(i)
+			if i not in conjunto2:
+				return False
+		return True
+
+	
+	def conjuntopropriamente(self, conjunto1, conjunto2):
+		aux = False
+
+		for i in conjunto1:
+			#print(i)
+			if i not in conjunto2:
+				return False
+		aux = True
+
+		if aux == True:
+			if conjunto1 == conjunto2:
+				return False
+		return True
+			
+		
 	def imprimir(self):
-		print(f'{self.nome.upper()} = {self.lista}')
+		lista = str(self.lista)
+		lista=lista.replace('[','{')
+		lista=lista.replace(']','}')
+		return f'{self.nome} = {lista}'
+		#print(f'{self.nome.upper()} = {self.lista}')
 	
 		
 #####################################
--- mainColecao.py
+++ mainColecao.py
@@ -11,11 +11,21 @@ c1.inserirElemento('A')
 print("Tamanho da Coleção:",c1.tamanhoConjunto())
 c1.imprimir()
 
-c2.inserirElemento('D')
-c2.inserirElemento(c1.lista)
+c2.inserirElemento('A')
+#c2.inserirElemento(c1.lista)
 
-print("Tamanho da Coleção:",c2.tamanhoConjunto())
-c2.imprimir()
+#print("Tamanho da Coleção:",c2.tamanhoConjunto())
+#c2.imprimir()
+c1.inserirElemento(c2.conjunto())
+print(c1.imprimir())
+
+print(c1.pertence('A', c1.conjunto()))
+
+print("c1:",c1.conjunto())
+print("c2:",c2.conjunto())
+print(c1.eh_subconjunto(c2.conjunto(), c1.conjunto()))
+print("#################top")
+print(c1.conjuntopropriamente(c2.conjunto(), c1.conjunto()))
 
 #####################################
 
